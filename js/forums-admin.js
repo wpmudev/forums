51b6e507101ee374818e1fbcb3673881b47b536a
moha <moha@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-04-26 22:00:23 +0000

    Fix JavaScript error
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/forums@3251 f53fe46e-8c02-459b-9fdd-93e75771b278

--- js/forums-admin.js
+++ js/forums-admin.js
@@ -7,20 +7,22 @@
 
 (function ($) {
 	$(document).ready(function () {
-	    $('#forum_color_one_panel').farbtastic('#forum_color_one').hide();
-            $('#forum_color_two_panel').farbtastic('#forum_color_two').hide();
-            $('#forum_color_header_panel').farbtastic('#forum_color_header').hide();
-            $('#forum_color_border_panel').farbtastic('#forum_color_border').hide();
-            
-            $("input.forum_color").focus(function() {
-                if ($(this).val() == '') {
-                    $(this).val('#ffffff');
-                }
-		$("#"+$(this).attr('id')+"_panel").slideDown();
-	    });
-            
-            $("input.forum_color").blur(function() {
-		$("#"+$(this).attr('id')+"_panel").slideUp();
-	    });
+            if ($('#forum_color_one').length > 0) {
+                $('#forum_color_one_panel').farbtastic('#forum_color_one').hide();
+                $('#forum_color_two_panel').farbtastic('#forum_color_two').hide();
+                $('#forum_color_header_panel').farbtastic('#forum_color_header').hide();
+                $('#forum_color_border_panel').farbtastic('#forum_color_border').hide();
+                
+                $("input.forum_color").focus(function() {
+                    if ($(this).val() == '') {
+                        $(this).val('#ffffff');
+                    }
+                    $("#"+$(this).attr('id')+"_panel").slideDown();
+                });
+                
+                $("input.forum_color").blur(function() {
+                    $("#"+$(this).attr('id')+"_panel").slideUp();
+                });
+            }
 	});
 })(jQuery);
