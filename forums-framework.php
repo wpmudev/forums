fedac80e82fb10ce8fe75158fd56adf3d3134ad7
moha <moha@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-04-26 22:03:43 +0000

    Schedule release
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/forums@3252 f53fe46e-8c02-459b-9fdd-93e75771b278

--- forums-framework.php
+++ forums-framework.php
@@ -5,7 +5,7 @@ Plugin URI: http://premium.wpmudev.org/project/forums
 Description: Allows each blog to have their very own forums - embedded in any page or post.
 Author: S H Mohanjith (Incsub), Ulrich Sossou (Incsub), Andrew Billits (Incsub)
 Author URI: http://premium.wpmudev.org
-Version: 1.6.6
+Version: 1.6.7
 Text Domain: wpmudev_forums
 WDP ID: 26
 */
@@ -27,7 +27,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-$forums_current_version = '1.6.6';
+$forums_current_version = '1.6.7';
 //------------------------------------------------------------------------//
 //---Config---------------------------------------------------------------//
 //------------------------------------------------------------------------//
